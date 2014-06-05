2e476aecb65c21fa125802c2c7b883c738b35371
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-06-05 15:46:20 -0700

    Can change rating

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ app.directive('myRating', function () {
     return {
       restrict: 'A',
       template: '<ul class="rating">' +
-                  '<li ng-repeat="star in stars" class="filled">' +
+                  '<li ng-repeat="star in stars" ng-class="star">' +
                     '\u2605' +
                   '</li>' +
                 '</ul>',
@@ -13,10 +13,19 @@ app.directive('myRating', function () {
         max: '='
       },
       link: function (scope, elem, attrs) {
-        scope.stars = [];
-        for (var  i = 0; i < scope.max; i++) {
-          scope.stars.push({filled: i < scope.ratingValue});
-        }
+
+        var updateStars = function() {
+          scope.stars = [];
+          for (var  i = 0; i < scope.max; i++) {
+            scope.stars.push({filled: i < scope.ratingValue});
+          }
+        };
+
+        scope.$watch('ratingValue', function(oldVal, newVal) {
+          if (newVal) {
+            updateStars();
+          }
+        });
       }
     }
   });
