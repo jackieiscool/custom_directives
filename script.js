2286d45ce79c6e7a406e999e741351242dca91a5
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-06-05 15:27:56 -0700

    created and called directive

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 var app = angular.module('myApp', [])
+
+app.directive('myRating', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, elem, attrs) {
+      console.log("Recognized the my-rating directive usage");
+    }
+  }
+});
