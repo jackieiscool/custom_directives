2a99783e01d82fd0239121f98e1baba19dde016c
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-06-05 15:36:01 -0700

    hmm

--- script.js
+++ script.js
@@ -9,12 +9,13 @@ app.directive('myRating', function () {
                   '</li>' +
                 '</ul>',
       scope: {
-        ratingValue: '='
+        ratingValue: '=',
+        max: '='
       },
       link: function (scope, elem, attrs) {
         scope.stars = [];
-        for (var  i = 0; i < scope.ratingValue; i++) {
-          scope.stars.push({});
+        for (var  i = 0; i < scope.max; i++) {
+          scope.stars.push({filled: i < scope.ratingValue});
         }
       }
     }
