b67703c45cbaec56567e0beccffaee1ea8ec2ba8
Jackie Herrlin <j.m.herrlin@gmail.com>
2014-06-05 15:34:13 -0700

    bug in ng-init

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var app = angular.module('myApp', [])
 
 app.directive('myRating', function () {
-  return {
-    restrict: 'A',
-    link: function (scope, elem, attrs) {
-      console.log("Recognized the my-rating directive usage");
+    return {
+      restrict: 'A',
+      template: '<ul class="rating">' +
+                  '<li ng-repeat="star in stars" class="filled">' +
+                    '\u2605' +
+                  '</li>' +
+                '</ul>',
+      scope: {
+        ratingValue: '='
+      },
+      link: function (scope, elem, attrs) {
+        scope.stars = [];
+        for (var  i = 0; i < scope.ratingValue; i++) {
+          scope.stars.push({});
+        }
+      }
     }
-  }
-});
+  });
